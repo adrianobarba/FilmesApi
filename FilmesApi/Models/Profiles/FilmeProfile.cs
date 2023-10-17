62ed4041148c16e83394ce83d29fa2b8277b6161
Adrianobarba <adrianobarbosa_4@hotmail.com>
2023-10-17 10:04:47 -0300

    commit inicial

---
+++ FilmesApi/Models/Profiles/FilmeProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using FilmesApi.Data.Dtos;
+
+namespace FilmesApi.Models.Profiles;
+
+public class FilmeProfile : Profile
+{
+
+    public FilmeProfile() 
+    {
+        CreateMap<CreateFilmeDto, Filme>();
+        CreateMap<UpdateFilmeDto, Filme>();
+        CreateMap<Filme, UpdateFilmeDto>();
+        CreateMap<Filme, ReadFilmeDto>();
+    }
+
+}
